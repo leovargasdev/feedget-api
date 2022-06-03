4b4d50b74addcd92ffc6fec7eead0753b02aeb4b
Leonardo Vargas <leu1607@hotmail.com>
2022-06-02 22:31:33 -0300

    feat: adicionando rota para listar os feedbacks

--- src/routes.ts
+++ src/routes.ts
@@ -3,8 +3,18 @@ import express from 'express'
 import { SubmitFeedbackService } from './services/submitFeedbackService';
 import { PrismaFeedbacksRepository } from './repositories/prisma/prisma-feedbacks-repository';
 import { NodemailerMailAdapter } from './adapters/nodemailer/nodemailer-mail-adapter';
+import { prisma } from './prisma';
 
-export const routes = express.Router()
+const routes = express.Router()
+
+routes.get('/feedbacks', async (req, res) => {
+  const feedbacks = await prisma.feedback.findMany()
+
+  return res.json({
+    total: feedbacks.length,
+    feedbacks,
+  })
+})
 
 routes.post('/feedbacks', async (req, res) => {
   const nodemailerMailAdapter = new NodemailerMailAdapter()
@@ -15,3 +25,5 @@ routes.post('/feedbacks', async (req, res) => {
 
   return res.status(201).send()
 })
+
+export { routes }
